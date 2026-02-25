92a5f082e644a3c81c765d666ba1c5734f3fd192
Aliplayer002 <aligholizade538@gmail.com>
2026-02-25 12:38:27 +0330

    payment request  page

--- src/components/RightNav.jsx
+++ src/components/RightNav.jsx
@@ -33,12 +33,7 @@ const IcBalance = () => (
   </svg>
 );
 
-const IcDaily = () => (
-  <svg viewBox="0 0 24 24" className={svgCls} fill="none" stroke="currentColor" strokeWidth={sw}>
-    <rect x="3" y="4" width="18" height="18" rx="2" />
-    <path d="M8 2v4M16 2v4M3 10h18" />
-  </svg>
-);
+const IcDaily = () => <img src="/images/icons/calendar.svg" className={icImgCls} alt="" draggable={false} />;
 
 function RightNav() {
   const { user } = useAuth();
