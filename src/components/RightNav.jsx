51b87e39f83718620a23166c4a025212263657da
Aliplayer002 <aligholizade538@gmail.com>
2026-08-24 10:17:46 +0330

    payment requset and tankhah

--- src/components/RightNav.jsx
+++ src/components/RightNav.jsx
@@ -310,7 +310,7 @@ function RightNav() {
           items: [
             { to: "/knowledge-management/equipment-library", label: "کتابخانه‌ها", hint: "کتابخانه‌ها", icon: <NavIcon src="/images/icons/ketabkhane.svg" /> },
             { to: "/knowledge-management/project-lessons-learned", label: "درس آموخته‌ها", hint: "درس آموخته‌ها", icon: <NavIcon src="/images/icons/darsamokhteha.svg" /> },
-            { to: "/knowledge-management/training-resources", label: "منابع آموزشی", hint: "محتوا و منابع آموزشی", icon: <NavIcon src="/images/icons/manabeamozeshi.svg" /> },
+            { to: "/knowledge-management/training-resources", label: "منابع آموزشی", hint: "محتوا و منابع آموزشی", icon: <span className="scale-[1.16]"><NavIcon src="/images/icons/manabeamozeshi.svg" /></span> },
           ],
         },
       ],
